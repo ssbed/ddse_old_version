dd89694703723ce3472e620230e27539be3aac65
ssbed <127472310+ssbed@users.noreply.github.com>
2024-04-26 11:03:41 +0900

    Update GUI_old_version.py

--- GUI_old_version.py
+++ GUI_old_version.py
@@ -10,4 +10,4 @@ pio.templates.default = "plotly"
 finger_ion = "👉"
 
 st.markdown("<div style='text-align: center; font-size: 18.4px;'><a href='https://www.li-lab-cat-design.com/'>Hao Li Lab@Advanced Institute for Materials Research (WPI-AIMR), Tohoku University, Japan</a></div>", unsafe_allow_html=True)
-st.markdown("<div style='text-align: left; font-size: 30px; color: red;'>Please click <a href='https://www.li-lab-cat-design.com/'>here</a> to access our new site</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: left; font-size: 30px; color: red;'>Please click <a href='https://ddse-database.streamlit.app/'>here</a> to access our new site</div>", unsafe_allow_html=True)
