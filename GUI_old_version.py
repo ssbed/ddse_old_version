ba66004c534797c71fb64e81481a189b5a6e3f67
ssbed <127472310+ssbed@users.noreply.github.com>
2024-04-26 10:08:20 +0900

    Update GUI_old_version.py

--- GUI_old_version.py
+++ GUI_old_version.py
@@ -9,5 +9,5 @@ st.image(title_image)
 pio.templates.default = "plotly"
 
 st.markdown("<div style='text-align: center; font-size: 18.4px;'><a href='https://www.li-lab-cat-design.com/'>Hao Li Lab@Advanced Institute for Materials Research (WPI-AIMR), Tohoku University, Japan</a></div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; font-size: 20px; color: red;'>Please click <a href='https://www.li-lab-cat-design.com/'>here</a> to access our new site</div>", unsafe_allow_html=True)
 
-st.markdown("<div style='text-align: center; font-size: 30px; color: red;'>Please click <a href='https://www.li-lab-cat-design.com/'>here</a> to access our new site</div>", unsafe_allow_html=True))
