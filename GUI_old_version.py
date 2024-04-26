4fd99bf311174162ec23ae804a7f0fed1a45718c
ssbed <127472310+ssbed@users.noreply.github.com>
2024-04-26 10:25:48 +0900

    Update GUI_old_version.py

--- GUI_old_version.py
+++ GUI_old_version.py
@@ -7,7 +7,8 @@ from plotly.subplots import make_subplots
 title_image = "./title.png"
 st.image(title_image)
 pio.templates.default = "plotly"
-finger_ion = :point_right:
+finger_ion = 👉
+
 st.markdown("<div style='text-align: center; font-size: 18.4px;'><a href='https://www.li-lab-cat-design.com/'>Hao Li Lab@Advanced Institute for Materials Research (WPI-AIMR), Tohoku University, Japan</a></div>", unsafe_allow_html=True)
 st.markdown("<div style='text-align: left; font-size: 30px; color: red;'>Please click <a href='https://www.li-lab-cat-design.com/'>here</a> to access our new site</div>", unsafe_allow_html=True)
 st.markdown("<div style='text-align: center; font-size: 20px; color: red;'>finger_ion; <a href='https://www.li-lab-cat-design.com/'>Please click here to access our new site</a></div>", unsafe_allow_html=True)
